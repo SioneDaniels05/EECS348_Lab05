168990553d82a04fe06c9c01aeef26ad09f2014d
SioneDaniels05 <143524119+SioneDaniels05@users.noreply.github.com>
2024-10-14 04:26:02 -0500

    Add files via upload

--- Software_Lab5/Project2.c
+++ Software_Lab5/Project2.c
@@ -20,25 +20,23 @@ printf("-----\n");//formating
 }
 
 //Adds the matrix's together
-void add(int m1[SIZE][SIZE], int m2[SIZE][SIZE], int sum[SIZE][SIZE]){
+void add(int m1[SIZE][SIZE], int m2[SIZE][SIZE]){
+	int sum_matrix[SIZE][SIZE];
 	for (int y = 0; y < SIZE; y++){
 		for (int x = 0; x < SIZE; x++){
-			sum[y][x] = (m1[y][x] + m2[y][x]); //adds the two values in the matrix's together and assigns them to the position of sum matrix
+			sum_matrix[y][x] = (m1[y][x] + m2[y][x]); //adds the two values in the matrix's together and assigns them to the position of sum matrix
 		}
 	}
 	printf("-----\nAddition: \n");//formating
-	print_matrix(sum);
+	print_matrix(sum_matrix);
 }
 
 //multiplies the matrix's
-void multi_matrix(int m1[SIZE][SIZE],int m2[SIZE][SIZE],int multi[SIZE][SIZE]){
+void multi_matrix(int m1[SIZE][SIZE],int m2[SIZE][SIZE]){
+	int multi[SIZE][SIZE];
 	for (int y = 0; y < SIZE; y++){
                 for (int x = 0; x < SIZE; x++){
 			multi[y][x] = 0;
-		}
-	}
-	for (int y = 0; y < SIZE; y++){
-		for (int x = 0; x < SIZE; x++){
 			for (int z = 0; z < SIZE; z++){//third for loop is used to properly multiply matrix's
 				multi[y][x] += (m1[y][z] * m2[z][x]);
 			}
@@ -74,14 +72,13 @@ int main(){//Main handles the input of the matrix's and checks if they're empty 
 		{10, 9, 8, 7, 6},
 		{5, 4, 3, 2, 1}
 		};
-	int sum[SIZE][SIZE];
 	int multi[SIZE][SIZE];
 	if ((m1 == NULL) || (m2 == NULL)){ //checks if they're empty
 		printf("Invalid Inputed Matrix");
 		return 0;
 	}
-	add(m1, m2, sum); //calls the add function
-	multi_matrix(m1, m2, multi); //calls the multi function
+	add(m1, m2); //calls the add function
+	multi_matrix(m1, m2); //calls the multi function
 	printf("Transpose: Matrix m1\n");
 	transpose_matrix(m1);//calls the transpose function
 	printf("Transpose: Matrix m2\n");
